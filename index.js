30075580c953f0e8f4c671ab9ebbcd9edcd4eff5
Jose Julian Salazar Melgar <josejuliansalazarmelgar@gmail.com>
2021-09-12 23:46:52 -0600

    Mongo Atlas

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose =  require("mongoose")
 const app = require("./app")
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/dbHotel', { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
+mongoose.connect('mongodb+srv://Admin:123añs@hoteles.q0szh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
     console.log('Se encuentra conectado a la base de datos');
 
     app.listen(process.env.PORT || 3000, function () {
